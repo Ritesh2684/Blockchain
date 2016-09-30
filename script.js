2f3d14edde2d4c9966e3473b7c5faa4b4c961d0c
sitaramgarimella <gsitaram.nist@gmail.com>
2016-09-30 17:48:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-var app = angular.module('myApp', []);
-	app.controller('myCtrl', function($scope, $http) {
-		$scope.query=function($scope, $http) {
-			alert();
-							
-			$http({
-				method : "GET",
-				url : "http://127.0.0.1:3000/query/ru/UK"
-			  }).then(function mySucces(response) {
-					alert("success REsponse:"+response);
-				  $scope.myWelcome = response.data;
-				  alert($scope.myWelcome );
-				}, function myError(response) {
-					alert("failure REsponse:"+response);
-				  $scope.myWelcome = response.statusText;
-				  alert($scope.myWelcome)
-			  });	
 
-		  
-		};
-	});
+
+		
+	   var app = angular.module('myApp', []);
+		app.controller('myCtrl',['$scope','$http', function($scope, $http) {
+			$scope.query=function($scope, $http) {
+				alert();
+								
+				$http({
+					method : "GET",
+					url : "http://127.0.0.1:3000/query/ru/UK"
+				  }).then(function mySucces(response) {
+						alert("success REsponse:"+response);
+					  $scope.myWelcome = response.data;
+					  alert($scope.myWelcome );
+					}, function myError(response) {
+						alert("failure REsponse:"+response);
+					  $scope.myWelcome = response.statusText;
+					  alert($scope.myWelcome)
+				  });	
+
+			  
+			};
+		}]);
 
 
 
